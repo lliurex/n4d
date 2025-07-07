19e326f36d70ce91f4c665f14f393cd52e24a6eb
cless <hectorgh@gmail.com>
2025-07-07 10:57:35 +0200

    fixed calls

--- built-in/httpmanager/delete_download.py
+++ built-in/httpmanager/delete_download.py
@@ -6,7 +6,7 @@ def delete_download(self,auth,section,file_path,delete_file=False):
 	ret=self.builtin_validation(auth,allowed_groups)
 	
 	if ret["status"]==0:	
-		return self.core.http_manager.delete_download(section,file_path,delete_file)
+		return self.http_manager.delete_download(section,file_path,delete_file)
 	else:
 		return ret
 
